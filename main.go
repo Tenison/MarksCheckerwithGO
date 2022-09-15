099fb27bbc40c8542910279eb0b1a56ba41bcc6e
Tenison <tenisonoseiowusu@gmail.com>
2022-09-15 13:29:35 +0000

    Marks Checker Program completed

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+
+func main (){
+	var marks int
+	fmt.Println("Please enter your marks")
+	fmt.Scanf("%d\n", &marks)
+
+	if upper := 70; marks >= upper {
+		fmt.Printf("You scored %v, exellent job", marks)
+	}else if average := 40; marks >= average{
+		fmt.Printf("You scored %v, Good work", marks)
+	}else{
+		fmt.Printf("You scored %v, Failed, try again", marks)
+	}
+}
